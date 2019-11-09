0d568f6b1f68152e7bf947adc1356b89a60814cb
Kai <kaipeng.wu@virginpulse.com>
2019-11-09 11:38:48 +0800

    Initial Project

---
+++ Cubic Weight/Cubic Weight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cubic Weight
+//
+//  Created by Kaipeng Wu on 9/11/19.
+//  Copyright © 2019 Kaipeng Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
